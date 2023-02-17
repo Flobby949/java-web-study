fbde81c85dec9f2b63559399f920fad6f63ecb2f
jinchenxing <2541226493@qq.com>
2023-02-17 16:50:27 +0800

    userService

---
+++ book-system/src/main/java/top/flobby/book/service/UserService.java
@@ -0,0 +1,11 @@
+package top.flobby.book.service;
+
+/**
+ * @author : JinChenXing
+ * @program : java-web-learning
+ * @description :
+ * @create : 2023-02-17 16:47
+ **/
+
+public interface UserService {
+}
---
+++ book-system/src/main/java/top/flobby/book/service/impl/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package top.flobby.book.service.impl;
+
+import top.flobby.book.service.UserService;
+
+/**
+ * @author : Flobby
+ * @program : java-web-learning
+ * @description :
+ * @create : 2023-02-17 16:47
+ **/
+
+public class UserServiceImpl implements UserService {
+}
